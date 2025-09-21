19c18a005ae53d3e84be0b9945352ac62e04a359
Bivol Nikita <titanov.tor@mail.ru>
2025-09-21 18:54:15 +0300

    Задание 2

---
+++ TZ1/src/Main.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+class CompareAndOperate {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Введите число a: ");
+        int a = scanner.nextInt();
+        System.out.print("Введите число b: ");
+        int b = scanner.nextInt();
+        if (a > b) {
+            System.out.println("a > b");
+        } else if (a < b) {
+            System.out.println("a < b");
+        } else {
+            System.out.println("a = b");
+        }
+
+        System.out.println("a + b = " + (a + b));
+        System.out.println("a - b = " + (a - b));
+        System.out.println("a * b = " + (a * b));
+        if (b != 0) {
+            System.out.println("a / b = " + (a / b));
+        } else {
+            System.out.println("Деление на ноль невозможно");
+        }
+
+        scanner.close();
+    }
+}
